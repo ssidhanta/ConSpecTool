65bf1ec1ece809257f8c6c197718be747d0fcdce
SUBHAJIT SIDHANTA <subhojitsidhanta2003@gmail.com>
2017-05-30 01:31:50 +0100

    pass

--- callConspecChecker.sh
+++ callConspecChecker.sh
@@ -1,9 +1,18 @@
-java -jar SpinPreProcess.jar "<1,w,x,1>,<2,w,x,2>,<3,r,x,2>,<4,r,x,1>"
-
-echo $1
-
-spin -a conspec$1Viol.pml
-
-gcc -o pan pan.c
-
-./pan -a
+#java -jar SpinPreProcess.jar ”<1,w,x,1>,<2,w,x,2>,<3,r,x,2>,<4,r,x,1>" $1
+SECONDS=0
+index=0
+size=5
+#index1=$(echo index | wc -l /Users/subhajitsidhanta/Downloads/Spin/training_data.arff)
+index=$(wc -l < /Users/subhajitsidhanta/Downloads/Spin/training_data.arff)
+index="$(expr $index / $size)"
+for i in `seq 0 $index`
+do
+    SECONDS=0
+    java -jar SpinPreProcess.jar $1 $i $size
+    spin -a conspec$1Viol.pml
+    gcc -o pan pan.c
+    ./pan -a
+    #echo “****after spin run: $i”
+    duration=$SECONDS
+    echo "$((duration / 3600)) minutes and $((duration % 3600)) seconds execution time."
+done
