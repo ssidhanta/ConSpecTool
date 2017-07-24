a7d922f2d62eab8096a773bc00ce3ec939b702da
SUBHAJIT SIDHANTA <subhojitsidhanta2003@gmail.com>
2017-07-24 15:39:22 +0100

    update millisecond diff prinupdate millisecond diff printt

--- callConspecChecker.sh
+++ callConspecChecker.sh
@@ -1,7 +1,7 @@
 #java -jar SpinPreProcess.jar ”<1,w,x,1>,<2,w,x,2>,<3,r,x,2>,<4,r,x,1>" $1
 #SECONDS=0
-STARTTIME=$(gdate +%s.%N) 
-STARTTIME=${STARTTIME%.*}
+STARTTIME=$(($(gdate +%s%N)/1000000)) 
+#STARTTIME=${STARTTIME%.*}
 #time a_command
 index=0
 size=5
@@ -11,16 +11,18 @@ index="$(expr $index / $size)"
 for i in `seq 0 $index`
 do
     #SECONDS=0
+    STARTTIME=$(($(gdate +%s%N)/1000000)) 
+    #STARTTIME=${STARTTIME%.*}
     java -jar SpinPreProcess.jar $1 $i $size
     spin -a conspec$1Viol.pml
     gcc -o pan pan.c
     ./pan -a
     #echo “****after spin run: $i”
     #duration=$SECONDS
-    ENDTIME=$(gdate +%s.%N)
+    ENDTIME=$(($(gdate +%s%N)/1000000))
     #echo "$((duration / 3600)) seconds and $((duration % 3600)) milliseconds execution time."
     #TIMEDIFF=`echo "$ENDTIME - $STARTTIME" | bc | awk -F"." '{print $1"."subset($2,1,3)}'`
-    ENDTIME=${ENDTIME%.*}
+    #ENDTIME=${ENDTIME%.*}
     DIFF=$((ENDTIME - STARTTIME))
     #DIFF=${DIFF%%.*}
     #echo $((DIFF)) | xargs printf "%.*f\n" $p
