6861e467d46c6bedd69f1761e35dd1ad843c09f0
SUBHAJIT SIDHANTA <subhojitsidhanta2003@gmail.com>
2019-11-20 19:03:24 +0530

    Update callConspecChecker.sh

--- callConspecChecker.sh
+++ callConspecChecker.sh
@@ -1,6 +1,6 @@
 #java -jar SpinPreProcess.jar ”<1,w,x,1>,<2,w,x,2>,<3,r,x,2>,<4,r,x,1>" $1
 #SECONDS=0
-STARTTIME=$((10#$(gdate +%s)/1000000))
+STARTTIME=$((10#$(date +%s)/1000000))
 #STARTTIME=$(($(gdate +%s%N)/1000000))
 #STARTTIME=${STARTTIME%.*}
 #time a_command
@@ -12,17 +12,17 @@ index="$(expr $index / $size)"
 for i in `seq 0 $index`
 do
     #SECONDS=0
-    STARTTIME=$(($(gdate +%s)/1000000))
+    STARTTIME=$(($(date +%s)/1000000))
     #STARTTIME=$(($(gdate +%s%N)/1000000))
     #STARTTIME=${STARTTIME%.*}
     java -jar SpinPreProcess.jar $1 $2 #$i $size
-    #spin -a conspec$2Viol.pml
-    spin -a burkhardt$2Viol.pml
+    spin -a conspec$2Viol.pml
+    #spin -a burkhardt$2Viol.pml
     gcc -o pan pan.c
     ./pan -a
     #echo “****after spin run: $i”
     #duration=$SECONDS
-    ENDTIME=$(($(gdate +%s%N)/1000000))
+    ENDTIME=$(($(date +%s%N)/1000000))
     #echo "$((duration / 3600)) seconds and $((duration % 3600)) milliseconds execution time."
     #TIMEDIFF=`echo "$ENDTIME - $STARTTIME" | bc | awk -F"." '{print $1"."subset($2,1,3)}'`
     #ENDTIME=${ENDTIME%.*}
